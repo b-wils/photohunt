8cedfb57e64385318f8cb25306634405bb3280cd
Brandon <bmw8719@gmail.com>
2019-05-02 12:45:09 -0500

    Add redux-immutable-state-invariant

--- redux/configureStore.js
+++ redux/configureStore.js
@@ -1,14 +1,20 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 
 import photos from './photos'
 
 export default function configureStore() {
 
+	const middleware = [];
+
+	if (process.env.NODE_ENV !== "production") {
+		middleware.push(require("redux-immutable-state-invariant").default());
+	}
+
 	let rootReducer = combineReducers ({
 		photos: photos
 	})
 
-	const store = createStore(rootReducer)
+	const store = createStore(rootReducer, applyMiddleware(...middleware))
 
 	return store;
 }
